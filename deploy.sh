f3ec193cefd7d97f492941c2cc61bcb11f07b534
sparanoid <info@sparanoid.com>
2011-02-08 06:34:16 +0800

    UPDATE deploy settings

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jekyll --no-server --no-auto && rsync -avz --delete --exclude=static/css/a.css /Users/sparanoid/dropbox/sparanoid-web/sparanoid.com/-/ root@209.114.36.40:/srv/www/sparanoid.com/public_html/-/
+jekyll --no-server --no-auto && rsync -avz --delete --exclude=static/css/a.css --exclude=lab /Users/sparanoid/dropbox/sparanoid-web/sparanoid.com/jekyll/ root@209.114.36.40:/srv/www/sparanoid.com/public_html/
