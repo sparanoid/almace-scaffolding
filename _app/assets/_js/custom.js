aa640bf509c16c36bcab89d7a88227b8aa9c7735
Tunghsiao Liu <t@sparanoid.com>
2015-09-02 16:53:21 +0800

    feat(grunt): preprocess leading quotes

--- _app/assets/_js/custom.js
+++ _app/assets/_js/custom.js
@@ -1,16 +1,1 @@
 // Custom user scripts
-
-// Quote marks fix
-// TODO: need better implement
-+function addMarginFix() {
-  var para = document.querySelectorAll("p, li");
-
-  for (var i = 0; i < para.length; i++) {
-    var firstLetter = para[i].innerHTML.trim().charAt(0);
-    var regex = /「|『|“/;
-
-    if (firstLetter.match(regex)) {
-      para[i].className += ' ' + "leading-indent-fix";
-    }
-  }
-}();
